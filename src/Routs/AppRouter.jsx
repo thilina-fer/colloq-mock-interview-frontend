663fcb6033eae7209d406a3fe78f4579f74fde9b
thilina-fer <dilshanthilina1010@gmail.com>
2026-02-24 07:49:14 +0530

    refactor: remove Bookings route from AppRouter for cleanup

--- src/Routs/AppRouter.jsx
+++ src/Routs/AppRouter.jsx
@@ -4,8 +4,6 @@ import LandingPage from "../pages/LandingPage";
 import CandidateProfile from "../pages/CandidateProfile";
 import InterviwerProfile from "../pages/InterviwerProfile";
 import AuthPage from "../pages/Auth ";
-import Bookings from "../pages/bookings";
-
 function AppRouter() {
   return (
     <BrowserRouter>
@@ -14,7 +12,6 @@ function AppRouter() {
         <Route path="/candidate-profile" element={<CandidateProfile />} />
         <Route path="/interviewer-profile" element={<InterviwerProfile />} />
         <Route path="/auth" element={<AuthPage />} />
-        <Route path="/bookings" element={<Bookings />} />
       </Routes>
     </BrowserRouter>
   );
